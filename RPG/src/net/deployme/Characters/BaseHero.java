8e1b98cf5ae8d6f9c54372860236bc146cc666b5
Rares Preda <rares.preda98@gmail.com>
2016-05-04 18:58:04 +0300

    RPG

---
+++ RPG/src/net/deployme/Characters/BaseHero.java
@@ -0,0 +1,103 @@
+package net.deployme.Characters;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseHero {
+    public enum Artifact {
+        SHIELD, GREATSWORD, STAFF, BOW, SWORD, CURSEDARMOR, OVER9K //also artifacts heal you to max hp
+        //over 9k instant 9001 damage
+        //shield - 20% damage taken
+        //greatsword + 30% damage dealt
+        //staff + 25% damage dealt + 20 ability power
+        //bow + 25% damage dealt + 40 agility
+        //sword +20% damage
+        //cursedarmor -50% damage taken BEFORE everything else
+    }
+    public enum HeroProfession {
+        RANGER, ELEMENTALIST, WARRIOR, GUARDIAN
+    }
+
+    //Fields have to be initialised by hero type
+    protected int hitpoints;
+    protected int maxHp;
+    protected int baseDamage;
+    protected int level = 1;
+    protected int armor; //1 point of armor reduces 0.2 damage ( /5 when computing)
+
+    protected HeroProfession profession;
+    private List<Artifact> ownedArtifacts = new ArrayList<Artifact>();
+
+    public String toString() {
+        String artifacts = "";
+        for (Artifact art : ownedArtifacts)
+            artifacts += art.toString() + " ";
+        if (ownedArtifacts.isEmpty())
+            artifacts = "none";
+        return profession.toString() + " HP: " + hitpoints + " LEVEL: " +
+                level + " ARTIFACTS: " + artifacts + " ARMOR: " + armor +
+                " BASEDMG: " + baseDamage;
+    }
+
+    protected abstract boolean canTakeArtifact(Artifact art);
+
+    public int dealDamage() {
+        int retVal = baseDamage;
+        retVal += retVal * 0.5 * level;
+        if (ownedArtifacts.contains(Artifact.OVER9K))
+            return 9001;
+        if (ownedArtifacts.contains(Artifact.GREATSWORD))
+            retVal += 0.3 * retVal;
+        if (ownedArtifacts.contains(Artifact.BOW))
+            retVal += 0.25 * retVal;
+        if (ownedArtifacts.contains(Artifact.SWORD))
+            retVal += 0.2 * retVal;
+        if (ownedArtifacts.contains(Artifact.STAFF))
+            retVal += 0.25 * retVal;
+
+        return retVal;
+    }
+
+    public  void levelDown() {
+        if (level > 1) {
+            level--;
+            maxHp = level * 100;
+            baseDamage -= 10;
+            if (hitpoints > maxHp)
+                hitpoints = maxHp;
+        }
+    }
+
+    public void levelUp() {
+        level ++;
+        maxHp = level * 100;
+        baseDamage += 10;
+    }
+
+    public boolean getArtifact(Artifact art) {
+        if (canTakeArtifact(art) && !ownedArtifacts.contains(art)) {
+            ownedArtifacts.add(art);
+            hitpoints = maxHp;
+            return true;
+        }
+        else return false;
+    }
+
+    private int computeDefense(int trueDamage) {
+        if (ownedArtifacts.contains(Artifact.CURSEDARMOR))
+            trueDamage /= 2;
+        if (ownedArtifacts.contains(Artifact.SHIELD))
+            trueDamage -= trueDamage * 0.2;
+        trueDamage -= armor / 5;
+        return trueDamage;
+    }
+
+    public boolean takeDamage(int trueDamage) {//returns true if still alive or false if dead
+        hitpoints -= computeDefense(trueDamage);
+        if (hitpoints <= 0) {
+            hitpoints = 0;
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ RPG/src/net/deployme/Characters/Ranger.java
@@ -0,0 +1,27 @@
+package net.deployme.Characters;
+
+/**
+ * Created by rpreda on 04/05/16.
+ */
+public class Ranger extends BaseHero {
+
+    protected boolean canTakeArtifact(BaseHero.Artifact art) {
+        switch (art) {
+            case BOW: return true;
+            case CURSEDARMOR: return true;
+            case OVER9K: return true;
+            case SWORD: return true;
+            default: return false;
+        }
+    }
+
+    public Ranger() {
+        profession = HeroProfession.RANGER;
+        hitpoints = 100;
+        maxHp = 100;
+        armor = 15;
+        baseDamage = 11;
+        level = 1;
+
+    }
+}
